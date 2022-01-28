4bea98ea947719e2e56145e9a41e4157331e9f84
Hormat Omar <hormatomar@gmail.com>
2022-01-28 13:48:23 -0500

    latest final commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,17 @@ public class Main {
     public static void main(String[] args) {
 //	the basic structure
         CarBasics car =new CarBasics();
+<<<<<<< Updated upstream
         car.color="blue";
         car.number="8658";
         car.engine="699954cc";
         car.door="4";
+=======
+        car.color="green";
+        car.number="789";
+        car.engine="54444cc";
+        car.door="3";
+>>>>>>> Stashed changes
         System.out.println(car.color);
     }
 }
