7b890be5063ff817dbefe31fe27ef3e497fe0ff2
Hormat Omar <hormatomar@gmail.com>
2022-01-28 08:48:27 -0500

    The basic structure of car is ready without any features

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	the basic structure
+        CarBasics car =new CarBasics();
+        car.color="red";
+        car.number="88877375";
+        car.engine="543543543554cc";
+        car.door="8";
+        System.out.println(car.color);
+    }
+}
---
+++ src/com/company/headlights.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class headlights {
+    String power;
+    String color;
+    String size;
+
+}
